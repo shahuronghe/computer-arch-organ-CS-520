e913a93b0e5ead89fb23a71f8967c2f6a1b83cf3
Shahu Ronghe <sronghe1@binghamton.edu>
2022-04-08 13:05:25 -0400

    cao academics update

---
+++ project01/project01.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int bakery_time;
+int no_request;
+int bagel_baked;
+int baguette_baked;
+int baking_count;
+float performance;
+int thousandCount = 0;
+int baking_limit = 0;
